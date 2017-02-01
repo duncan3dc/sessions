4df5017886aa019a56c1f9ccacb1571e845df57a
Thomas Flori <tflori@pb-systems.de>
2017-02-01 05:51:24 +0100

    fix session save path for webtest

--- tests/web/sub/bootstrap.php
+++ tests/web/sub/bootstrap.php
@@ -5,4 +5,10 @@ namespace duncan3dc\Sessions;
 require __DIR__ . "/../../../vendor/autoload.php";
 
 session_set_cookie_params(3600, '/sub', 'localhost', false, true);
+
+session_save_path('/tmp/duncan3dc-sessions');
+if (!file_exists(session_save_path())) {
+    mkdir(session_save_path());
+}
+
 $session = new SessionInstance("web");
