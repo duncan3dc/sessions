cc39af6554387e4e58ac7fbec4611beabf63985b
Craig Duncan <git@duncanc.co.uk>
2025-05-08 13:38:56 +0100

    Add support for PHP 8.3

--- tests/Storageless/MiddlewareTest.php
+++ tests/Storageless/MiddlewareTest.php
@@ -2,17 +2,21 @@
 
 namespace duncan3dc\SessionsTest\Storageless;
 
+use Dflydev\FigCookies\SetCookie;
 use duncan3dc\Sessions\SessionInterface;
 use duncan3dc\Sessions\Storageless\Middleware;
 use duncan3dc\Sessions\Storageless\Session;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\ServerRequest;
 use Laminas\Stratigility\MiddlewarePipe;
+use Lcobucci\JWT\Configuration as JwtConfig;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Signer\Key\InMemory;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use PSR7Sessions\Storageless\Http\Configuration;
 use PSR7Sessions\Storageless\Http\SessionMiddleware;
 
 use function Laminas\Stratigility\middleware;
@@ -21,7 +25,19 @@ class MiddlewareTest extends TestCase
 {
     private function getSession(): Middleware
     {
-        $middleware = SessionMiddleware::fromSymmetricKeyDefaults(InMemory::plainText("mBC5v1sOKVvbdEitdSBenu59nfNfhwkedkJVNabosTw="), 60);
+        $middleware = new SessionMiddleware(
+            (new Configuration(
+                JwtConfig::forSymmetricSigner(
+                    new Sha256(),
+                    InMemory::plainText("mBC5v1sOKVvbdEitdSBenu59nfNfhwkedkJVNabosTw="),
+                ),
+            ))->withCookie(
+                SetCookie::create('an-example-cookie-name')
+                    ->withSecure(false)
+                    ->withHttpOnly(true)
+                    ->withPath('/'),
+            )->withIdleTimeout(60),
+        );
 
         return new Middleware($middleware);
     }
